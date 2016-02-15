f166b40b9a99ddf02b10e47a7f59360c2f7553a3
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-02-15 12:14:13 -0800

    Restructure server file structure to include /config directory

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ var port = process.env.PORT || 1337
 mongoose.connect('mongodb://localhost/nanza');
 
 require('./config/middleware.js')(app, express);
-//require('./config/routes.js')(app, express);
+require('./config/routes.js')(app, express);
 
 app.listen(port);
 
