df9cf1b9b66cbb3f58ae3c262674ac6d30c8eece
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-02-15 14:36:42 -0800

    Build out mongoose schema for users

--- server/db/questionizerModels.js
+++ server/db/questionizerModels.js
@@ -1,5 +1,15 @@
 var mongoose = require('mongoose')
 
-var QuestionStorage = new mongoose.schema({
+var PromptStorage = new mongoose.schema({
   question: String
 });
+
+var UserSchema = new mongoose.schema({
+  username: String,
+  submission: String,
+  votes: Number,
+  timestamp: Date
+});
+
+module.exports = mongoose.model('prompts', PromptStorage);
+module.exports = mongoose.model('user', UserSchema);
