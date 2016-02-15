c1ad5a557d7c4f5d570a5f1e1d3cf497151afbbc
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-02-15 12:09:09 -0800

    Reconfigure server to use process.env and fallback to localhost

--- server/server.js
+++ server/server.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
 var app = express();
+
+var port = process.env.PORT || 1337
 
 mongoose.connect('mongodb://localhost/nanza');
 
 require('./config/middleware.js')(app, express);
 //require('./config/routes.js')(app, express);
 
-app.listen(1337);
-
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/www/index.html')
-});
+app.listen(port);
 
 module.exports = app;
