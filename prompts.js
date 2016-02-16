234057a0c80e12eb1ddea2471114da37620c167a
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-02-15 22:53:31 -0800

    Add 10 fun prompts in a JSON file

---
+++ prompts.js
@@ -0,0 +1,14 @@
+{
+  "prompts": [
+    "Weirdest music video",
+    "Most mind-blowing GIF",
+    "Worst \"It's a prank, bro!\" video",
+    "Most tantalizing Food Porn GIF",
+    "Craziest comment on Donald Trump's Facebook page",
+    "Most badass music video",
+    "That weird part of YouTube",
+    "Coolest sports moment",
+    "Cheesiest 90s song",
+    "The most oddly-specific music genre you can find on YouTube"
+  ]
+}
