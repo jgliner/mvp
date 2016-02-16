20d9c5359543181c4c890690ca8ccd2e40c0cc30
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-02-15 22:31:03 -0800

    Add brief countdown in server once all players are present
    Makes it easier to see exactly when Angular's view is changing

--- server/config/util.js
+++ server/config/util.js
@@ -4,8 +4,21 @@ module.exports = function(io) {
   io.on('connection', function(client) {
     userCount+=1
     console.log('Client conencted. Users in room: ', userCount);
+
     client.once('client connected', function() {
       io.emit('user joined', userCount);
+      if (userCount === 2) { // manually change for more/fewer players
+          var countdown = 2;
+        setInterval(function() {
+          countdown--;
+          console.log(countdown)
+          if (countdown === 0) {
+            console.log('GO!')
+            io.emit('start game', userCount);
+            clearInterval(this);
+          }
+        }, 1000);
+      }
     });
   });
 
