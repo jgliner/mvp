1d41ff137f5c1ec9ecbd500fe6b57d923ec609d4
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-02-16 15:54:45 -0800

    Add styling for waiting room

--- client/controllers/waiting.js
+++ client/controllers/waiting.js
@@ -2,9 +2,15 @@ angular.module('pocket-bonanza.waitingPage', ['pocket-bonanza.socket'])
 
 .controller('waitingCtrl', ['$scope', '$state', 'socket', function($scope, $state, socket) {
   $scope.connect = socket.socketEmit('client connected');
+
   socket.socketOn('user joined', function(count) {
     $scope.userCount = count;
+    $scope.$apply(function() {
+      $scope.players = 2-count === 1 ? 'Player' : 'Players';
+      $scope.userCountLabel = 2-count === 1 ? 'Other Waiting' : 'Others Waiting';
+    })
   });
+
   socket.socketOn('start round', function() {
     $state.go('/main');
   });
