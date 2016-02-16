9a2f9fb0515137c2dc57176b10c2df86b4603815
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-02-15 16:17:07 -0800

    Add JSON body-parser to middleware
    Clean up routes

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,6 +1,6 @@
-var questionizer = require('../db/questionizerControllers.js')
+var questionizer = require('../db/questionizerControllers.js');
 
 module.exports = function(app, express) {
-  app.get('/api/questions/', questionizer.pickOne);
-  app.post('/api/questions/', questionizer.addNew);
+  app.get('/api/users/', questionizer.pickOne);
+  app.post('/api/users/', questionizer.addNewUser);
 };
