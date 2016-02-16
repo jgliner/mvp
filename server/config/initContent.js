dfb0e7cefe0ed94eda634a1da5433c3ba53bc63e
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-02-16 10:11:24 -0800

    Read from prompts.js in preparation for mongoose pre-population

---
+++ server/config/initContent.js
@@ -0,0 +1,19 @@
+var Promise = require('bluebird');
+var fs = require('fs');
+var mongoose = require('mongoose');
+var db = require('../db/questionizerControllers.js');
+var path = require('path');
+
+module.exports = function(cb) {
+  return new Promise(function(resolve, reject) {
+    fs.readFile(path.join(__dirname+'/../../prompts.js'), 'utf8', function(err, data) {
+      if (err) {reject(err);}
+      else {
+        resolve(data)
+      }
+    })
+  })
+  .then(function(data) {
+    cb(data)
+  })
+}
--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,7 +1,13 @@
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
+var populateQuestions = require('./initContent.js');
 
 module.exports = function(app, express) {
+  app.use(function(req, res, next) {
+    populateQuestions(function() {
+      next();
+    });
+  });
   app.use(morgan('dev'));
   app.use(bodyParser.json());
   app.use(express.static(__dirname + '/../../client'));
