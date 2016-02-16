b9aa21e868000fd55744854826a414c4cf64b66e
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-02-16 13:45:57 -0800

    Fill in results directive and filter out own submission

--- client/controllers/results.js
+++ client/controllers/results.js
@@ -1,5 +1,19 @@
 angular.module('pocket-bonanza.resultsPage', ['pocket-bonanza.socket'])
 
 .controller('resultsCtrl', ['$scope', '$rootScope', 'socket', function($scope, $rootScope, socket) {
-  
+  $scope.notOwnUsername = function(sub) {
+    return sub.username !== $rootScope.username;
+  }
 }])
+.directive('answer', function() {
+  return {
+    template:
+    `
+    <div ng-repeat="ans in ansObj | filter:notOwnUsername">
+      <h2>{{ans.username}}</h2>
+      <h3>{{ans.submission}}</h3>
+    </div>
+
+    `
+  }
+})
