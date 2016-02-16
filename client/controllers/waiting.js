d093673ad3f3a0d0247fefff9368333c09ce02e2
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-02-15 21:38:31 -0800

    Initialize waitingPage module and add connections to socket service

--- client/controllers/waiting.js
+++ client/controllers/waiting.js
@@ -1,5 +1,8 @@
-angular.module('pocket-bonanza.waitingPage', [])
+angular.module('pocket-bonanza.waitingPage', ['pocket-bonanza.socket'])
 
-.controller('waitingCtrl', ['$scope', function($scope) {
-  $scope.thing = 'sup'
+.controller('waitingCtrl', ['$scope', 'socket', function($scope, socket) {
+  $scope.connect = socket.socketEmit('client connected');
+  socket.socketOn('user joined', function(count) {
+    $scope.userCount = count;
+  })
 }])
