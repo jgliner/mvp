29e985c165f39adb8ccb62b04d66064cfb029fbc
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-02-15 22:41:11 -0800

    Add state change for waiting room when server sends "start round"

--- client/controllers/waiting.js
+++ client/controllers/waiting.js
@@ -1,8 +1,11 @@
 angular.module('pocket-bonanza.waitingPage', ['pocket-bonanza.socket'])
 
-.controller('waitingCtrl', ['$scope', 'socket', function($scope, socket) {
+.controller('waitingCtrl', ['$scope', '$state', 'socket', function($scope, $state, socket) {
   $scope.connect = socket.socketEmit('client connected');
   socket.socketOn('user joined', function(count) {
     $scope.userCount = count;
-  })
+  });
+  socket.socketOn('start round', function(count) {
+    $state.go('/main');
+  });
 }])
