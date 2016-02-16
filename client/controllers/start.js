a4563bcb6bf7ee78eb6d294c5f69ef12eccc7087
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-02-15 19:10:02 -0800

    Connect start page controller to factory function that is invoked on form submission

--- client/controllers/start.js
+++ client/controllers/start.js
@@ -1,5 +1,14 @@
 angular.module('pocket-bonanza.startPage', [])
 
-.controller('startCtrl', function($scope) {
-  
-})
+.controller('startCtrl', ['$scope', 'userFactory', function($scope, userFactory) {
+  $scope.user = {name: ''};
+  $scope.submitName = function(user) {
+    userFactory(user);
+  }
+}])
+
+.factory('userFactory', [function(userObj) {
+  return function(userObj) {
+    console.log(userObj.name);
+  }
+}])
