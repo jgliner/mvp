c024a665259d56b3d8839664603dbc8347e5729c
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-02-15 16:59:24 -0800

    Add bower and frontend dependencies
    For some reason, bower is not paying attention to "ignore". Used .gitignore instead

--- server/server.js
+++ server/server.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
 var app = express();
 
-var port = process.env.PORT || 1337
+var port = process.env.PORT || 1337;
 
 mongoose.connect('mongodb://localhost/nanza');
 
